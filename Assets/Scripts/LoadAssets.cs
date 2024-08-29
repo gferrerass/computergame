6c91d0b5dbbba26831feb93ce3f29aa3d8c35176
gferrerass <842741@unizar.es>
2024-08-29 13:45:41 +1000

    70 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -6,11 +6,20 @@ using UnityEngine;
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
-
+    [SerializeField]
+    private GameObject blueObj;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // create a redObj at (2,0,0) and a rotation of zero
+        Instantiate(redObj,new Vector3(2, 0, 0), Quaternion.identity);
+        // create a blueObj at (-2,0,0) and a rotation of zero
+        Instantiate(blueObj,new Vector3(-2, 0, 0), Quaternion.identity);
+
+        // Limit framerate to cinematic 60 fps.
+        QualitySettings.vSyncCount = 0; // Set vSyncCount to 0 so that using .targetFrameRate is enabled.
+        Application.targetFrameRate = 60;
+
     }
 
     // Update is called once per frame
